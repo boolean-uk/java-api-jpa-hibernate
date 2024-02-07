2726ac2f954159b7d0c3ebfdb4db2294718024d3
Petros Mylonas <petrosmylonascs@gmail.com>
2024-02-07 19:49:00 +0200

    Petros Mylonas

---
+++ src/main/java/com/booleanuk/api/model/Game.java
@@ -0,0 +1,86 @@
+package com.booleanuk.api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "games")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "developer")
+    private String developer;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    @Column(name = "is_early_access")
+    private boolean isEarlyAccess;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public boolean isEarlyAccess() {
+        return isEarlyAccess;
+    }
+
+    public void setEarlyAccess(boolean earlyAccess) {
+        isEarlyAccess = earlyAccess;
+    }
+}
