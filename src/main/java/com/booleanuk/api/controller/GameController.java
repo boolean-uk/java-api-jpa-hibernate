67d0cd3069ddd135a5f854a96734a2da6252eb9f
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-02-07 13:17:29 +0100

    Removed import statement Added application.yml duplicate in gitignore

--- src/main/java/com/booleanuk/api/controller/GameController.java
+++ src/main/java/com/booleanuk/api/controller/GameController.java
@@ -4,7 +4,6 @@ import com.booleanuk.api.model.Game;
 import com.booleanuk.api.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
