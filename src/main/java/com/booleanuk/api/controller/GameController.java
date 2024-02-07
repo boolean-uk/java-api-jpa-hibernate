7102f6a64ca444e22e65d0e54cff68aa0c19fffe
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-07 13:11:33 +0100

    Create getAll games endpoint

--- src/main/java/com/booleanuk/api/controller/GameController.java
+++ src/main/java/com/booleanuk/api/controller/GameController.java
@@ -1,9 +1,15 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.model.Game;
+import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("games")
@@ -11,4 +17,9 @@ public class GameController {
 
     @Autowired
     private GameRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Game>> getAll() {
+        return ResponseEntity.ok(this.repository.findAll());
+    }
 }
