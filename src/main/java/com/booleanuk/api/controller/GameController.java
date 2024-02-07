7788358c204796860ef42ad8b86332269c88a381
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-07 13:10:52 +0100

    Set up GameController class

--- src/main/java/com/booleanuk/api/controller/GameController.java
+++ src/main/java/com/booleanuk/api/controller/GameController.java
@@ -1,4 +1,14 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("games")
 public class GameController {
+
+    @Autowired
+    private GameRepository repository;
 }
