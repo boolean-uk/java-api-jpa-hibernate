2c72331e179a6fe61515d3d6598e6d90dbbdc481
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-02-07 13:01:45 +0100

    Updated application.yml file Created api classes for User and Game

--- src/main/java/com/booleanuk/api/controller/UserController.java
+++ src/main/java/com/booleanuk/api/controller/UserController.java
@@ -2,36 +2,64 @@ package com.booleanuk.api.controller;
 
 import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
-    private final UserRepository repository;
-
-    public UserController(UserRepository repository) {
-        this.repository = repository;
-    }
+    @Autowired
+    private UserRepository repository;
 
     @GetMapping
-    public List<User> getAll() {
-        return this.repository.findAll();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(this.repository.findAll());
     }
 
+    /**
     @GetMapping("{id}")
-    public User getById(@PathVariable("id") Integer id) {
+    public User getUserById(@PathVariable("id") Integer id) {
         return this.repository.findById(id).orElseThrow();
     }
 
     record PostUser(String email, String firstName) {}
-
     @ResponseStatus(HttpStatus.CREATED)
+     */
     @PostMapping
-    public User create(@RequestBody PostUser request) {
-        User user = new User(request.email(), request.firstName());
-        return this.repository.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        //User userToBeCreated = new User(user.getEmail(), user.getFirstName(), user);
+        return new ResponseEntity<>(this.repository.save(user), HttpStatus.CREATED);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User user) {
+        User userToBeUpdated = findUser(id);
+
+        userToBeUpdated.setEmail(user.getEmail());
+        userToBeUpdated.setFirstName(user.getFirstName());
+        userToBeUpdated.setLastName(user.getLastName());
+        userToBeUpdated.setUserName(user.getUserName());
+        userToBeUpdated.setPhone(user.getPhone());
+
+        User updatedUser = this.repository.save(userToBeUpdated);
+
+        return new ResponseEntity<>(updatedUser, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable int id) {
+        User userToBeDeleted = findUser(id);
+        this.repository.deleteById(id);
+        return ResponseEntity.ok(userToBeDeleted);
+    }
+
+    private User findUser(int id) {
+        return this.repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't find user"));
+    }
+
 }
--- src/main/java/com/booleanuk/api/model/User.java
+++ src/main/java/com/booleanuk/api/model/User.java
@@ -1,78 +1,49 @@
 package com.booleanuk.api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name="users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name="email")
     private String email;
+
+    @Column(name="first_name")
     private String firstName;
-    private Boolean isActive;
 
-    public User(Integer id, String email, String firstName, Boolean isActive) {
-        this.id = id;
-        this.email = email;
-        this.firstName = firstName;
-        this.isActive = isActive;
-    }
+    @Column(name="last_name")
+    private String lastName;
 
-    public User(String email, String firstName) {
-        this.email = email;
-        this.firstName = firstName;
-        this.isActive = false;
-    }
+    @Column(name="user_name")
+    private String userName;
 
-    public Integer getId() {
-        return id;
-    }
+    @Column(name="phone")
+    private String phone;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(firstName, user.firstName) && Objects.equals(isActive, user.isActive);
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(userName, user.userName) && Objects.equals(phone, user.phone);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, email, firstName, isActive);
+        return Objects.hash(id, email, firstName, lastName, userName, phone);
     }
 
     @Override
@@ -81,7 +52,9 @@ public class User {
                 "id=" + id +
                 ", email='" + email + '\'' +
                 ", firstName='" + firstName + '\'' +
-                ", isActive=" + isActive +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", phone=" + phone +
                 '}';
     }
 }
