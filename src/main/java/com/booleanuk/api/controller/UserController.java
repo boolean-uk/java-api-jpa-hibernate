aac02043be788ece440b19b9ac2db703ef9c86af
Alexander Niklasson <alexander.niklasson@live.com>
2024-02-07 12:22:18 +0100

    Exercise complete

--- src/main/java/com/booleanuk/api/controller/UserController.java
+++ src/main/java/com/booleanuk/api/controller/UserController.java
@@ -2,36 +2,66 @@ package com.booleanuk.api.controller;
 
 import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
-    private final UserRepository repository;
-
-    public UserController(UserRepository repository) {
-        this.repository = repository;
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public List<User> getAll() {
-        return this.repository.findAll();
+        return this.userRepository.findAll();
     }
 
     @GetMapping("{id}")
     public User getById(@PathVariable("id") Integer id) {
-        return this.repository.findById(id).orElseThrow();
+        return this.userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    record PostUser(String email, String firstName) {}
-
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public User create(@RequestBody PostUser request) {
-        User user = new User(request.email(), request.firstName());
-        return this.repository.save(user);
+    public ResponseEntity<User> create(@RequestBody User user) {
+        try {
+            return new ResponseEntity<>(this.userRepository.save(user), HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateOneUser(@PathVariable int id, @RequestBody User user) {
+        User userToUpdate = this.userRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (user.getFirstName() == null || user.getLastName() == null
+                || user.getPhone() == null || user.getUsername() == null || user.getEmail() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        userToUpdate.setFirstName(user.getFirstName());
+        userToUpdate.setLastName(user.getLastName());
+        userToUpdate.setUsername(user.getUsername());
+        userToUpdate.setPhone(user.getPhone());
+        userToUpdate.setEmail(user.getEmail());
+
+        return new ResponseEntity<>(this.userRepository.save(userToUpdate), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteOneUser(@PathVariable int id){
+        User userToDelete = this.userRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        this.userRepository.delete(userToDelete);
+        return new ResponseEntity<>(userToDelete, HttpStatus.ACCEPTED);
+    }
+
+
 }
