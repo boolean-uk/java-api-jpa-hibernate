d47e12292f0fef36a6b234bd041acc1c3d9ab980
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-07 13:05:38 +0100

    Use autowired for dependency injection

--- src/main/java/com/booleanuk/api/controller/UserController.java
+++ src/main/java/com/booleanuk/api/controller/UserController.java
@@ -2,6 +2,7 @@ package com.booleanuk.api.controller;
 
 import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping("users")
 public class UserController {
-    private final UserRepository repository;
-
-    public UserController(UserRepository repository) {
-        this.repository = repository;
-    }
+    @Autowired
+    private UserRepository repository;
 
     @GetMapping
     public ResponseEntity<List<User>> getAll() {
