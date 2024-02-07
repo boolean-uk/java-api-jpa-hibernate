7b85bbf07c79c63fdab17e4896b20c6a4047283c
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-02-07 15:34:58 +0100

    Using lombok in classes

--- src/main/java/com/booleanuk/api/model/Game.java
+++ src/main/java/com/booleanuk/api/model/Game.java
@@ -1,13 +1,15 @@
 package com.booleanuk.api.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "games")
 @Getter
 @Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Game {
     @Id
     @GeneratedValue
@@ -19,29 +21,4 @@ public class Game {
     private String developer;
     private int releaseYear;
     private boolean isEarlyAccess;
-
-    public Game() {
-    }
-
-    public Game(String title, String genre, String publisher, String developer, int releaseYear, boolean isEarlyAccess) {
-        this.title = title;
-        this.genre = genre;
-        this.publisher = publisher;
-        this.developer = developer;
-        this.releaseYear = releaseYear;
-        this.isEarlyAccess = isEarlyAccess;
-    }
-
-    @Override
-    public String toString() {
-        return "Game{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", genre='" + genre + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", developer='" + developer + '\'' +
-                ", releaseYear=" + releaseYear +
-                ", isEarlyAccess=" + isEarlyAccess +
-                '}';
-    }
 }
