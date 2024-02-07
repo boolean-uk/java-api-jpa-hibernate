d76a7a55d25631821476a8694d6ae82ac5bf34fe
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-07 13:36:30 +0100

    EXERCISE COMPLETE

--- src/main/java/com/booleanuk/api/controller/GameController.java
+++ src/main/java/com/booleanuk/api/controller/GameController.java
@@ -1,7 +1,6 @@
 package com.booleanuk.api.controller;
 
 import com.booleanuk.api.model.Game;
-import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
