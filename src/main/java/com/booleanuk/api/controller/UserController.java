3dc296e3785994f3e390b886e412525ba1d04529
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-02-09 16:25:00 +0100

    setup gradle + completed user restapi

--- src/main/java/com/booleanuk/api/controller/UserController.java
+++ src/main/java/com/booleanuk/api/controller/UserController.java
@@ -2,7 +2,9 @@ package com.booleanuk.api.controller;
 
 import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("users")
 public class UserController {
+    @Autowired
     private final UserRepository repository;
 
     public UserController(UserRepository repository) {
@@ -17,21 +20,42 @@ public class UserController {
     }
 
     @GetMapping
-    public List<User> getAll() {
-        return this.repository.findAll();
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.repository.findAll();
+        return ResponseEntity.ok(users);
     }
 
-    @GetMapping("{id}")
-    public User getById(@PathVariable("id") Integer id) {
-        return this.repository.findById(id).orElseThrow();
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Integer id) {
+        User user = this.repository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 
-    record PostUser(String email, String firstName) {}
-
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public User create(@RequestBody PostUser request) {
-        User user = new User(request.email(), request.firstName());
-        return this.repository.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User newUser = this.repository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Integer id, @RequestBody User user) {
+        if (!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        user.setId(id);
+        User updatedUser = repository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Integer id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
