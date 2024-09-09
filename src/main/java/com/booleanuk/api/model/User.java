c0ab926b9bd0abf7cc303ee6aa93149ea72f3e72
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-09 14:23:53 +0200

    added missing getters and setters in User model.

--- src/main/java/com/booleanuk/api/model/User.java
+++ src/main/java/com/booleanuk/api/model/User.java
@@ -69,6 +69,30 @@ public class User {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     public Boolean getActive() {
         return isActive;
     }
