5431154c23d5b353058faeca5a20f9f522afa3e8
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-24 15:15:23 +0100

    Finish Users

--- src/main/java/com/booleanuk/api/controller/UserController.java
+++ src/main/java/com/booleanuk/api/controller/UserController.java
@@ -3,7 +3,9 @@ package com.booleanuk.api.controller;
 import com.booleanuk.api.model.User;
 import com.booleanuk.api.repository.UserRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -26,12 +28,32 @@ public class UserController {
         return this.repository.findById(id).orElseThrow();
     }
 
-    record PostUser(String email, String firstName) {}
+//    record PostUser(String email, String firstName) {}
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public User create(@RequestBody PostUser request) {
-        User user = new User(request.email(), request.firstName());
+    public User create(@RequestBody User user) {
+//        User user = new User(request.getEmail(), request.getFirstName());
         return this.repository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User user) {
+        User userToUpdate = repository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No user with id")
+        );
+        userToUpdate.setFirstName(user.getFirstName());
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setActive(user.getActive());
+        return new ResponseEntity<>(userToUpdate, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> removeUser(@PathVariable int id) {
+        User user = repository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No user with id")
+        );
+        repository.deleteById(id);
+        return new ResponseEntity<>(this.repository.save(user), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/booleanuk/api/model/User.java
+++ src/main/java/com/booleanuk/api/model/User.java
@@ -17,6 +17,12 @@ public class User {
     @Column(name="first_name")
     private String firstName;
 
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="phone")
+    private String phone;
+
     @Column(name="is_active")
     private Boolean isActive;
 
